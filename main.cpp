9612546299e45263bfb4ac21ba5086f94abdece6
WitherVictor <number42@yeah.net>
2025-03-17 12:08:44 +0800

    开启服务器的端口从命令行给定

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from chatroom_server!\n";
+#include <spdlog/spdlog.h>
+
+#include "utility.hpp"
+
+void show_usage() {
+    std::cout <<
+    "可选参数：\n" <<
+    "    --port 端口号    在指定的端口上开放服务器"
+    << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc - 1 < 2) {
+        std::cout << "参数错误，请检查输入。" << std::endl;
+        show_usage();
+        return -1;
+    }
+
+    if (std::strcmp(argv[1], "--port") == 0) {
+        if (!valid_port(argv[2])) {
+            std::cout << "参数错误，请检查输入。\n"
+                      << "    错误：端口 [" << argv[2] << "] 不是合法端口！"
+                      << std::endl;
+            return -1;
+        }
+        
+
+    }
 }
