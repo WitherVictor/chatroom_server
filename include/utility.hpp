fa1bb76b233cf5794a1f936260412e8ef440b4e9
WitherVictor <number42@yeah.net>
2025-03-19 09:30:55 +0800

    添加函数 make_port，将 const char* 端口转化成 unsigned short

--- include/utility.hpp
+++ include/utility.hpp
@@ -4,9 +4,12 @@
 #include <cstring>
 #include <cctype>
 
+#include <charconv>
+
 #include <spdlog/spdlog.h>
 
 using cstring = const char*;
+using port = unsigned short;
 
 inline bool valid_port(cstring port_str) {
     spdlog::debug("开始验证端口: {}", port_str);
@@ -20,4 +23,10 @@ inline bool valid_port(cstring port_str) {
 
     spdlog::debug("端口 [{}] 是合法端口。", port_str);
     return true;
+}
+
+inline port make_port(cstring port_str) {
+    port result_port{};
+    std::from_chars(port_str, port_str + std::strlen(port_str), result_port);
+    return result_port;
 }
