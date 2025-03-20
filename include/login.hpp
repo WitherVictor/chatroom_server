af91c4330f343894456dd96e1c1af4fe7198adbf
WitherVictor <number42@yeah.net>
2025-03-20 16:17:51 +0800

    实现登录阶段的消息收发

--- include/login.hpp
+++ include/login.hpp
@@ -3,9 +3,12 @@
 #include <list>
 
 #include <boost/asio.hpp>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 
 #include "session.hpp"
 
+using json = nlohmann::json;
 using namespace boost::asio::ip;
 
 class login {
@@ -18,14 +21,44 @@ public:
         return instance;
     }
 
+    void process_login_request(std::shared_ptr<session> connection, const std::string& raw_json_data) {
+        try {
+            auto request_json = json::parse(raw_json_data);
+
+            auto request_type = request_json.at("request_type").get<std::string>();
+            if (request_type == "login") {
+                auto username = request_json.at("username").get<std::string>();
+                auto password = request_json.at("password").get<std::string>();
+
+                if (username == "123" && password == "456") {
+                    spdlog::info("用户验证通过！");
+                    json reply_json{};
+                    reply_json["request_type"] = "login";
+                    reply_json["result"] = "passed";
+
+                    auto raw_data = reply_json.dump() + session::packet_separator;
+                    connection->write(raw_data);
+                }
+            } else {
+                spdlog::error("登录模块接收的请求类型不匹配，实际请求类型：{}", request_type);
+            }
+        } catch (std::exception& ex) {
+            spdlog::error("处理登录请求错误！原因：{}", ex.what());
+        }
+    }
+
     void acquire_socket(tcp::socket socket) {
-        m_socket_list.emplace_back(session{std::move(socket)});
-        m_socket_list.back().read();
+        auto data_received_callback = [this](std::shared_ptr<session> connection, const std::string& data) {
+            this->process_login_request(connection, data);
+        };
+        auto new_session = std::make_shared<session>(std::move(socket), data_received_callback);
+        m_socket_list.emplace_back(new_session);
+        m_socket_list.back()->read();
     }
 private:
     login() = default;
 
     //  等待连接建立之后 socket 被转移到这里
     mutable std::mutex m_list_mutex;
-    std::list<session> m_socket_list;
+    std::list<std::shared_ptr<session>> m_socket_list;
 };
--- include/session.hpp
+++ include/session.hpp
@@ -1,37 +1,77 @@
 #pragma once
 
-#include "boost/asio/placeholders.hpp"
-#include "boost/system/detail/error_code.hpp"
 #include <cstddef>
-#include <array>
+#include <functional>
 
 #include <boost/asio.hpp>
-#include <boost/bind.hpp>
+#include <boost/bind/bind.hpp>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 
 using namespace boost;
 using namespace boost::asio::ip;
+using namespace boost::placeholders;
 
-class session {
+class session : public std::enable_shared_from_this<session> {
 public:
-    explicit session(tcp::socket socket)
-        : m_socket{std::move(socket)} {}
+    explicit session(tcp::socket socket, std::function<void(std::shared_ptr<session>, const std::string&)> callback)
+        : m_socket{std::move(socket)}, m_callback{std::move(callback)} {}
+
+    session(const session&) = delete;
+    session& operator=(const session&) = delete;
 
     void read() {
-        m_socket.async_receive(asio::buffer(m_buffer),
+        asio::async_read_until(m_socket,
+                               m_buffer,
+                               packet_separator,
                                boost::bind(&session::data_received,
-                                                    this,
-                                                    asio::placeholders::error,
-                                                    asio::placeholders::bytes_transferred));
+                                            this,
+                                            asio::placeholders::error,
+                                            asio::placeholders::bytes_transferred));
     }
 
-    void data_received(const boost::system::error_code& ec, std::size_t bytes_trasferred) {
+    void write(std::string data) {
+        m_socket.async_send(asio::buffer(data),
+                            boost::bind(&session::data_sent, 
+                                        this,
+                                        asio::placeholders::error,
+                                        asio::placeholders::bytes_transferred));
+    }
 
+    void data_sent(const boost::system::error_code& ec, std::size_t bytes_transferred) {
+        if (ec) {
+            spdlog::error("消息发送出现错误！错误码：{}，错误消息：{}", ec.value(), ec.message());
+            return;
+        }
+
+        spdlog::info("消息已发送，传输字节数：{}", bytes_transferred);
+    }
+
+    void data_received(const boost::system::error_code& ec, std::size_t bytes_transferred) {
+        //  处理错误
+        if (ec) {
+            spdlog::error("消息传输出现错误！错误码：{}，错误消息：{}", ec.value(), ec.message());
+            return;
+        }
+
+        //  将读入的数据存储到 std::string 中等待处理
+        std::istream stream(&m_buffer);
+        std::string raw_json_data{};
+        std::getline(stream, raw_json_data, packet_separator);
+        m_buffer.consume(bytes_transferred);
+
+        //  调用回调函数处理数据
+        m_callback(shared_from_this(), raw_json_data);
 
         read();
     }
 
-    inline static constexpr std::size_t buffer_size = 4096;
+    inline static constexpr char packet_separator = '\x1E';
 private:
-    std::array<std::byte, session::buffer_size> m_buffer;
+    boost::asio::streambuf m_buffer;
     tcp::socket m_socket;
+
+    //  接收到数据后调用回调函数处理数据
+    std::function<void(std::shared_ptr<session>, const std::string&)> m_callback;
 };
