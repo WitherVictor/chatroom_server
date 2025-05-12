0b182e151881ca5dd5e401335146f210d14201ec
WitherVictor <number42@yeah.net>
2025-05-12 16:42:05 +0800

    重构 database

--- include/database.hpp
+++ include/database.hpp
@@ -1,7 +1,6 @@
 #include <sqlite_orm/sqlite_orm.h>
 
 #include <string>
-#include <mutex>
 
 //  account 表结构
 struct account {
@@ -10,46 +9,19 @@ struct account {
     std::string password;
 };
 
-class database {
-public:
+namespace database {
     static constexpr auto file_path = "account.db";
 
-    //  单例对象禁止复制
-    database(const database&) = delete;
-    database& operator=(const database&) = delete;
+    inline auto storage() {
+        auto new_storage = sqlite_orm::make_storage(database::file_path,
+                                sqlite_orm::make_table("account",
+                                    sqlite_orm::make_column("id", &account::id, sqlite_orm::primary_key().autoincrement()),
+                                    sqlite_orm::make_column("name", &account::name),
+                                    sqlite_orm::make_column("password", &account::password)
+                                )
+                            );
 
-    //  亦不考虑移动
-    database(database&&) = delete;
-    database& operator=(database&&) = delete;
-
-    //  Meyers' Singleton
-    static database& instance() {
-        static database instance;
-        return instance;
+        new_storage.sync_schema();
+        return new_storage;
     }
-
-private:
-    //  默认构造函数与默认析构函数私有
-    database() = default;
-    ~database() = default;
-
-    static auto& storage() {
-        static auto storage = []() {
-            auto storage_instance = sqlite_orm::make_storage(database::file_path,
-                                        sqlite_orm::make_table("account",
-                                            sqlite_orm::make_column("id", &account::id, sqlite_orm::primary_key().autoincrement()),
-                                            sqlite_orm::make_column("name", &account::name),
-                                            sqlite_orm::make_column("password", &account::password)
-                                        )
-                                    );
-
-            //  与存储文件同步
-            storage_instance.sync_schema();
-            return storage_instance;
-        }();
-
-        return storage;
-    }
-
-    static std::mutex m_storage_mutex;
-};
+};  //  end of namespace database
